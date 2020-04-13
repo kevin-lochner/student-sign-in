5a0351d9a4e52309429a9a54f68b4c6089fcf22a
kevin-lochner <kevin.lochner@go.minneapolis.edu>
2020-04-13 14:00:28 -0500

    All functional

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
+
+    <StudentTable 
+    v-bind:students="students" 
+    v-on:student-present="studentArrivedOrLeft"
+    v-on:delete-student="studentDeleted"
+    ></StudentTable>
+
+    <StudentMessage v-bind:message="message" v-bind:name="name"></StudentMessage>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentTable from './components/StudentTable.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentRow from './components/StudentRow.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data() {
+    return {
+      students: [],
+      message: '',
+      name: '',
+    }
+  },
+    components: {
+    NewStudentForm,
+    StudentMessage,
+    StudentTable
+  },
+  methods: {
+    newStudentAdded(student) {
+    this.students.push(student)
+    this.students.sort(function(s1, s2) {
+      return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1 : 1
+      })
+    },
+    studentArrivedOrLeft(student) {
+      this.name = student.name
+      this.message = student.present ? 'Welcome' : 'Goodbye'
+    },
+    studentDeleted(student) {
+      this.students = this.students.filter(function(s) {return s != student })
+    } 
+  },
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/StudentRow.vue
@@ -0,0 +1,49 @@
+<template>
+    <tr v-bind:class="'present-' + student.present">
+        <td>>{{student.name}}</td>
+        <td>{{student.starID}}</td>
+        <td><input type="checkbox" v-model="student.present" v-on:change="checked(student)"></td>
+        <td v-show="editTable">
+            <img class="delete-icon" v-on:click="deleteStudent(student)" src="@/assets/delete.png">
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: "StudentRow",
+    props: {
+        student: Object,
+        editTable: Boolean
+    },
+    methods: {
+        checked(student) {
+            this.$emit('student-present', student)
+        },
+        deleteStudent(student) {
+            if(confirm(`Are you sure you want to delete ${student.name}?`)) {
+                this.$emit('delete-student', student)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+.present-true {
+    color: gray;
+    font-style: italic;
+}
+
+.present-false {
+    font-weight: bold;
+}
+
+
+.delete-icon {
+    height: 20px;
+}
+
+
+</style>
